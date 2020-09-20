18bd482a1f15d7acc630b3e35a738d95c21f3ce8
jaiden <jaiden@schraut.org>
2020-09-20 17:08:32 -0500

    Initial Commit

---
+++ Mod2_CreativeApp_CalculatorJaidenS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mod2_CreativeApp_CalculatorJaidenS
+//
+//  Created by Tiger Coder on 9/20/20.
+//  Copyright © 2020 clc.JXSchraut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
